afe0819c7411659619bc8e7d9b06e3c5f4a11091
Martin Ted <martin@bitboxx.com>
2023-03-08 10:59:32 +0000

    Rules update Wed  8 Mar 10:59:32 GMT 2023

--- babel-preset-react.js
+++ babel-preset-react.js
@@ -1,3 +1,17 @@
-module.exports = () => ({
-  presets: [require('@babel/preset-react')],
-});
+const PRESET_REACT = '@babel/preset-react';
+
+module.exports = (_, options = {}) => {
+  const presetReactOptions = options[PRESET_REACT] ?? {};
+
+  return {
+    presets: [
+      [
+        require(PRESET_REACT),
+        {
+          runtime: 'automatic',
+          ...presetReactOptions,
+        },
+      ],
+    ],
+  };
+};
